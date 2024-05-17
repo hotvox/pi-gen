41a791a03a328134b742b4c2cac7d56195dca396
numemac <ryan@augustinsky.ca>
2024-05-16 20:09:23 -0400

    setting network

--- stage3/00-install-hotsat/00-run.sh
+++ stage3/00-install-hotsat/00-run.sh
@@ -11,10 +11,15 @@ systemctl enable inputd.service
 systemctl enable outputd.service
 EOF
 
+echo "WPA_SSID=${WPA_SSID}"
+echo "WPA_PASSWORD=${WPA_PASSWORD}"
 # If WPA_SSID and WPA_PASSWORD are set, configure wlan
-if [ -v WPA_SSID ] && [ -v WPA_PASSWORD ]; then
+if [ -n "${WPA_SSID}" ] && [ -n "${WPA_PASSWORD}" ]; then
+    echo "Configuring network"
 	on_chroot <<- EOF
+        echo "Running set_wlan"
 		/usr/lib/raspberrypi-sys-mods/imager_custom set_wlan "${WPA_SSID}" "${WPA_PASSWORD}"
+        echo "Enabling NetworkManager service"
         systemctl enable NetworkManager.service
 	EOF
 fi
